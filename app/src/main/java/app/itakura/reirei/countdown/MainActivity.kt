525e17b9444f67c7fd6355e2c24067cb291f72bf
mirei <usrmii301@gmail.com>
2021-02-07 11:30:46 +0900

    tapして花が咲く

--- app/src/main/java/app/itakura/reirei/countdown/MainActivity.kt
+++ app/src/main/java/app/itakura/reirei/countdown/MainActivity.kt
@@ -3,13 +3,14 @@ package app.itakura.reirei.countdown
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
 
     var second = 10
-    val timer:CountDownTimer = object:CountDownTimer(10000,1000){
+    val timer: CountDownTimer = object : CountDownTimer(10000, 1000) {
         override fun onFinish() {
             startButton.isVisible = true
             second = 10
@@ -35,5 +36,29 @@ class MainActivity : AppCompatActivity() {
             timer.start()
         }
 
+        var count = 0
+
+        tapButton.setOnClickListener {
+            count += 1
+            when {
+                15>count -> {
+                    Log.d("count",count.toString())
+                    imageView.setImageResource(R.drawable.level3)
+                }
+                30>count -> {
+                    imageView.setImageResource(R.drawable.level2)
+                }
+                50>=count -> {
+                    imageView.setImageResource(R.drawable.level4)
+                }
+                60>=count -> {
+                    imageView.setImageResource(R.drawable.level6)
+                }
+                count > 60 -> {
+                    imageView.setImageResource(R.drawable.level5)
+                }
+            }
+
+        }
     }
 }
